8a15fa9a6eb87c3277684ddc2d732c96ab4a94a6
Tomasz Steć <tomeczekstecc@gmail.com>
2019-07-14 20:35:41 +0200

    f

--- server.js
+++ server.js
@@ -7,18 +7,18 @@ const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/authors");
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({limit: "10mb", extended:false}))
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true
+  useNewUrlParser: false
 });
 const db = mongoose.connection;
 db.on("error", error => console.error(error));
