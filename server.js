ec31534b924312ed6f4add7554b3f74916a4507e
Tomasz Steć <tomeczekstecc@gmail.com>
2019-07-14 20:43:34 +0200

    f

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: false
+  useNewUrlParser: true
 });
 const db = mongoose.connection;
 db.on("error", error => console.error(error));
