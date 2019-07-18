ad1998cd2ac5abd4b7d03a1e1b7f552bc1338720
Tomasz Steć <tomeczekstecc@gmail.com>
2019-07-18 21:47:20 +0200

    updfating, deleting autors

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const expressLayouts = require("express-ejs-layouts");
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/authors");
 const bookRouter = require("./routes/books");
+const methodOverride = require('method-override')
 
 const bodyParser = require("body-parser");
 
@@ -16,6 +17,7 @@ app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
+app.use(methodOverride('_method'))
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
@@ -30,5 +32,6 @@ db.once("open", () => console.log("Connected to Mangoose"));
 app.use("/", indexRouter);
 app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
+
 
 app.listen(process.env.PORT || 3000);
